8c54fce80883a30ada5bf3f6f4419685c5225ea3
Peter Wood <pw0470@gmail.com>
2021-02-27 09:28:39 +0000

    Added additional logging output.

--- src/ws_echo.cr
+++ src/ws_echo.cr
@@ -6,10 +6,12 @@ ws "/" do |socket|
   sockets.push socket
 
   socket.on_close do |_|
+    puts "Socket closed."
     sockets.delete(socket)
   end
 
   socket.on_message do |message|
+    puts "Received: #{message}"
     socket.send "ECHO: #{message}"
   end
 end
